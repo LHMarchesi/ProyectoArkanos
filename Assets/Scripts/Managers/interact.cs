1f9a4487ba65a1c32a249e4c46406b20e7be522c
LautaroMarchesi <lautarohmarchesi@gmail.com>
2024-05-19 14:08:50 -0300

    Intereaccion con npc y fixes para build

--- Assets/Scripts/Managers/interact.cs
+++ Assets/Scripts/Managers/interact.cs
@@ -2,17 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class interact : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Player player;
+    private bool isPlayerOnRange;
+
+    [SerializeField] private GameObject dialogueMark;
+    [SerializeField] private Dialogue dialogue;
+
+    private bool isDialogueActive = false;
+
     void Start()
     {
-        
+        player = GameObject.Find("Player").GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlayerOnRange && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!isDialogueActive)
+            {
+                StartDialogue();
+
+                if (dialogue.didDialogueEnd)
+                {
+                    EndDialogue();
+                    GameManager.Instance.IsEnemy(gameObject);
+                }
+            }
+        }
+    }
+
+
+    private void StartDialogue()
+    {
+        dialogue.StartDialogueSecuence();
+        player.PlayerCanMove(false);
+        dialogueMark.SetActive(false);
+
+    }
+
+    private void EndDialogue()
+    {
+        dialogue.EndDialogueSecuence();
+        player.PlayerCanMove(true);
+        isDialogueActive = false;
+        dialogueMark.SetActive(true);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) // Triggers para saber si esta dentro del rango
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerOnRange = true;
+            dialogueMark.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isPlayerOnRange = false;
+            dialogueMark.SetActive(false);
+
+        }
     }
 }
+
+
