aca28e3cbc5a9b6eabede0360200025d53b89c04
Fianriel <42074035+esteban18498@users.noreply.github.com>
2024-06-12 17:20:14 -0300

    Animacion / Timer Portal

--- Assets/Scripts/Player/PlayerOnPortal.cs
+++ Assets/Scripts/Player/PlayerOnPortal.cs
@@ -7,11 +7,22 @@ public class PlayerOnPortal : MonoBehaviour
 {
     [SerializeField] private Animator animatorPortal;
 
+
+    private void Start()
+    {
+        animatorPortal.SetBool("PlayerTrigger", false);
+    }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
+
         if (collider.gameObject.CompareTag("Player"))
         {
             animatorPortal.SetBool("PlayerTrigger", true);
+        }
+        else
+        {
+            animatorPortal.SetBool("PlayerTrigger", false) ;
         }
     }
 }
--- Assets/Scripts/Player/PlayerTP.cs
+++ Assets/Scripts/Player/PlayerTP.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using static Cinemachine.DocumentationSortingAttribute;
 using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
 
 public class PlayerTP : MonoBehaviour
 {
     [SerializeField] private Animator animatorPlayer;
     [SerializeField] private string Level;
+    [SerializeField] private float transitionScene;
+    [SerializeField] private float transitionFade;
+    [SerializeField] private Animator transitionAnimator;
 
+    private void Start()
+    {
+        transitionScene = 1.3f;
+        transitionFade = 0.5f;
+    }
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.CompareTag("Player"))
         {
             animatorPlayer.SetBool("PlayerTP", true);
-            ScenesLoader.instance.LoadScene(Level);
-
+            StartCoroutine(WaitForAnimation());
         }
+    }
+
+    public IEnumerator WaitForAnimation()
+    {
+        yield return new WaitForSecondsRealtime(transitionFade);
+        transitionAnimator.SetTrigger("StartTransition");
+        yield return new WaitForSecondsRealtime(transitionScene);
+        SceneManager.LoadScene(Level);
     }
 }
