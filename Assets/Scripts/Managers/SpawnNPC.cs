fb295d83c580eafd3d2fb95c9fe6eb6fd76c9484
Fianriel <42074035+esteban18498@users.noreply.github.com>
2024-06-12 15:20:41 -0300

    Nivel 1.1

--- Assets/Scripts/Managers/SpawnNPC.cs
+++ Assets/Scripts/Managers/SpawnNPC.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class SpawnNPC : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] private Animator animator;
     [SerializeField] private SpriteRenderer Gandalf;
+
+    private void Start()
+    {
+        Gandalf.enabled = false;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             animator.SetBool("PlayerColition", true);
+            Gandalf.enabled = true;
         }
     }
+
+    
 
 }
